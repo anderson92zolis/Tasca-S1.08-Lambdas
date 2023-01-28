9ea18210f800e318454c2e5f0e15c9ad87c085d9
anderson92zolis <azolis92@hotmail.es>
2023-01-29 00:58:12 +0100

    N1E6 HECHO

---
+++ src/n1exercici6/Main.java
@@ -0,0 +1,24 @@
+package n1exercici6;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) {	
+	/*
+	 * - Exercici 6
+	 * Crea una llista amb nombres i cadenes de text i ordena la llista amb les cadenes de 
+	 * més curta a més llarga.
+	 */
+		
+		List<String> myNumbers = Arrays.asList("2","5","si","6","7","hol","1","que","pasa");
+		
+		myNumbers.stream()
+		.sorted((String o1,String o2)-> o1.length()-o2.length())
+		.collect(Collectors.toList())
+		.forEach(System.out::print);
+		
+	}
+}
