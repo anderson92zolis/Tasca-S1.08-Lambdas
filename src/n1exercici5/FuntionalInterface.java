b7912da333d1ca9115b64160b7136b954d68b75d
anderson92zolis <azolis92@hotmail.es>
2023-01-28 20:27:55 +0100

    N1E5 hecho

---
+++ src/n1exercici5/FuntionalInterface.java
@@ -0,0 +1,6 @@
+package n1exercici5;
+
+public interface FuntionalInterface {
+
+	double getPiValue();
+}
---
+++ src/n1exercici5/Main.java
@@ -0,0 +1,14 @@
+package n1exercici5;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		FuntionalInterface myInterfacePi = () ->3.1415;
+		
+		System.out.println(myInterfacePi.getPiValue());
+		
+		
+	}
+
+}
