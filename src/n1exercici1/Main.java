66ce6ce9d0d05856bf2f23f8b32f939d80beced7
anderson92zolis <azolis92@hotmail.es>
2023-01-28 11:13:12 +0100

    N1E1 hecho

---
+++ src/n1exercici1/Main.java
@@ -0,0 +1,40 @@
+package n1exercici1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * - Exercici 1
+		 * A partir d’una llista de Strings, escriu un mètode que retorna una llista de totes les cadenes que contenen la lletra ‘o’. 
+		 * Imprimeix el resultat.
+
+		 */
+	
+		
+	ArrayList<String> llistaString = new ArrayList<String>(Arrays.asList("Volvo","BMW","Ford","Mazda"));
+	
+	containO(llistaString).forEach(wordWithO-> {
+		System.out.println("These are the words that contain 'o' :"+ wordWithO);
+		});
+	
+	}
+	
+	
+	
+	public static ArrayList<String> containO( ArrayList<String> llistaString){
+		
+		ArrayList<String> containO = (ArrayList<String>) llistaString.stream()
+				.filter(word->word.toLowerCase().contains("o"))
+				.collect(Collectors.toList());
+			
+		return containO;
+	}
+	
+	
+}
+
+
