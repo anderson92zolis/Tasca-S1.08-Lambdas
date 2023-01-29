0a076f00c21d71620aa06239cf036f43f53cd59a
anderson92zolis <azolis92@hotmail.es>
2023-01-29 01:04:59 +0100

    corrección de la impresión del N1E6, NIE7 hecho

--- src/n1exercici7/Main.java
+++ src/n1exercici7/Main.java
@@ -1,4 +1,4 @@
-package n1exercici6;
+package n1exercici7;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,10 +13,10 @@ public class Main {
 	 * més curta a més llarga.
 	 */
 		
-		List<String> myNumbers = Arrays.asList("2","5","si","6","7","hol","1","que","pasa");
+		List<String> myNumbers = Arrays.asList("1","2","A","BB","CCC","DDDD","1","FFFF","LLLLL");
 		
 		myNumbers.stream()
-		.sorted((String o1,String o2)-> o1.length()-o2.length())
+		.sorted((String o1,String o2)-> o2.length()-o1.length())
 		.collect(Collectors.toList())
 		.forEach(System.out::println);
 		
