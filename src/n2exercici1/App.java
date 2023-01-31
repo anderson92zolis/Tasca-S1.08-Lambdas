b1432b183d9b1f16f67047cd19a7781a3cb16f29
anderson92zolis <azolis92@hotmail.es>
2023-01-31 20:08:01 +0100

    n1n2 Hecho

--- src/n2exercici1/App.java
+++ src/n2exercici1/App.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class Devuelve {
+public class App {
 
 	public static void main(String[] args) {
 	
